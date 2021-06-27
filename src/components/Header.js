1ce6ba47123d6c4f67a01374b1cb663a7e0e8671
yiczhang2005 <yiczhang2005@gmail.com>
2021-06-27 08:31:44 -0400

    Modified H2 formate to double rows

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ const Header = () => {
     return (
         <header>
             <h1>Daily Task Manager</h1>
-            <h2>Record what you don't wanna do (but have to to) and clear them!</h2>
+            <h2>Record what you don't wanna do --</h2>
+            <h2> (but have to to) and clear them!</h2>
         </header>
     );
 };
